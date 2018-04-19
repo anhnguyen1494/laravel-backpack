3efca69a3b50c709d3d4bdd4800616bf721eb9c4
TuanAnh <anhnguyen1494@gmail.com>
2018-04-19 12:47:58 +0700

    update permission manager

--- app/User.php
+++ app/User.php
@@ -4,11 +4,14 @@ namespace App;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Backpack\CRUD\CrudTrait; // <------------------------------- this one
+use Spatie\Permission\Traits\HasRoles;// <---------------------- and this one
 
 class User extends Authenticatable
 {
     use Notifiable;
-
+    use CrudTrait; // <----- this
+    use HasRoles; // <------ and this
     /**
      * The attributes that are mass assignable.
      *
